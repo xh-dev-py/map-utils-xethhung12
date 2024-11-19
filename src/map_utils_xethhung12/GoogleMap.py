d21a1213c1afe3b5f9643467153078a3a95a8baa
xeth hung <xethhung@xh-network.xyz>
2024-11-19 16:53:16 +0800

    add address by latlon

--- src/map_utils_xethhung12/GoogleMap.py
+++ src/map_utils_xethhung12/GoogleMap.py
@@ -1,8 +1,33 @@
+import json
 import re
 
 import requests
 
 from map_utils_xethhung12.Locator import LatLon
+
+
+class Geocode:
+    def __init__(self, key):
+        self.key = key
+
+    def geocode_api_by_address(self, address: str, key: str)->dict:
+        LocationParams = {'address': address, key: key}
+        LocationURL = 'http://maps.googleapis.com/maps/api/geocode/json'
+        r = requests.get(LocationURL, params=LocationParams)
+        responseJson = json.loads(r.text)
+        self.res = responseJson
+        return self.res
+
+    def get_size_of_result(self):
+        return len(self.res)
+
+    def get_lat_lon_obj(self, i=0)->LatLon:
+        lat = float(self.res.get('results')[i]['geometry']['location']['lat'])
+        lng = float(self.res.get('results')[i]['geometry']['location']['lng'])
+        return LatLon(lat, lng)
+
+    def get_address(self, i=0) -> str:
+        return self.res.get('results')[i]['formatted_address']
 
 
 def get_url_from_latlon_object(latlon: LatLon) -> str:
